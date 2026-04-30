1f8b91aa8a3aa487c19917a1a571ad6871564b38
TorneselloGianluca <s348916@studenti.polito.it>
2026-04-30 17:50:47 +0200

    aggiunta file deploy

--- podisticaParabita/vite.config.js
+++ podisticaParabita/vite.config.js
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite' 
 
+
 export default defineConfig({
+  base: '/', 
   assetsInclude: ['**/*.mp4'],
   plugins: [
     react(),
